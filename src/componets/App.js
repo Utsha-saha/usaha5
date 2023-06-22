5ff2f105162f13f30c7db276c62c1626e6f52e81
Utsha5 <68023409+Utsha-saha@users.noreply.github.com>
2023-06-22 10:55:05 -0700

    Add files via upload

---
+++ src/componets/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Artist from './Artist';
+import Tracks from './Tracks';
+import Search from './Search';
+
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
+class App extends Component{
+  state = { artist : null, tracks: []};
+   
+  componentDidMount() {
+    this.searchArtist('Bruno Mars');
+  }
+  
+   searchArtist = artistQuery =>{
+    // console.log('this.state',this.state);
+    fetch(`${API_ADDRESS}/artist/${artistQuery}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log('json', json);
+
+      if(json.artists.total > 0) {
+        const artist = json.artists.items[0];
+
+        console.log('artist', artist);
+        this.setState({ artist });
+
+        fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+        .then(response => response.json())
+        .then(json => this.setState({tracks:json.tracks}))
+        .catch(error => alert(error.message));
+      }
+    })
+    .catch(error => alert(error.message));
+  }
+
+  
+
+  render() {
+    console.log('this.state',this.state);
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <Search searchArtist={this.searchArtist}/>
+        <Artist artist={this.state.artist}/>
+        <Tracks tracks={this.state.tracks} />
+      </div>
+    );
+  }
+}
+
+export default App;
